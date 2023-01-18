e404c54fd171ebe0e54d25a65e6fb4ba06959665
Fabiano Matos <fabiano.matoz@gmail.com>
2023-01-18 01:15:36 -0300

    torctl update * adjust firewall rules to down server on linux

--- config/fw-down-linux.sh
+++ config/fw-down-linux.sh
@@ -6,10 +6,10 @@
 # Created: Tuesday, 2023/01/17 - 01:45:02
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Tuesday, 2023/01/17 - 01:53:58
+# Last Modified: Tuesday, 2023/01/17 - 23:07:14
 # Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.1.3
+# Version: 0.0.1.4
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  >
@@ -22,12 +22,12 @@ fwLinuxDown(){
         iptables-restore $APP_HOME/iptables.rules.bkp
     else
         # clear all FW rules
-        iptables -P INPUT ACCEPT
-        iptables -P FORWARD ACCEPT
-        iptables -P OUTPUT ACCEPT
-        iptables -t nat -F
-        iptables -t mangle -F
-        iptables -F
+        iptables -P INPUT ACCEPT && \
+        iptables -P FORWARD ACCEPT && \
+        iptables -P OUTPUT ACCEPT && \
+        iptables -t nat -F && \
+        iptables -t mangle -F && \
+        iptables -F && \
         iptables -X
     fi
 }
